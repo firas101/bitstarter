8458d8571d5a858d4da30d9874e1722d7c2c7500
Ubuntu <ubuntu@ip-172-31-39-60.us-west-2.compute.internal>
2013-07-11 12:40:53 +0000

    Updated web.js 1

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var content = fs.readFileSync('index.html','utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(content.toString('utf8');
 });
 
 var port = process.env.PORT || 5000;
